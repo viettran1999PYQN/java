356e3faa6bc516cb282a68d8dba91c247ca573ab
unknown <viettranquoc24111999@gmail.com>
2022-02-08 22:15:13 -0800

    sua lan 3

--- hello1.java
+++ hello1.java
@@ -3,6 +3,7 @@ class hello1{
         System.out.print("hello");
         System.out.print("them 1 dong");
         System.out.println("them 2 dong ");
+        System.out.println("them 3 dong ");
         
     }
 }
