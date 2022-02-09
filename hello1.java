0ae495588966358baa550b720f89501a7634ff0c
unknown <viettranquoc24111999@gmail.com>
2022-02-08 22:07:27 -0800

    thay doi lan 2

--- hello1.java
+++ hello1.java
@@ -2,6 +2,7 @@ class hello1{
     public static void main(String[] args) {
         System.out.print("hello");
         System.out.print("them 1 dong");
+        System.out.println("them 2 dong ");
         
     }
 }
